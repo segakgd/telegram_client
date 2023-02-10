7e1710e9a3f46db982b39462530052ca3f05bfc6
sega <hpsergeyy@yandex.ru>
2023-02-10 23:30:11 +0200

    add method for response builder

--- src/HttpClient/ResponseBuilder.php
+++ src/HttpClient/ResponseBuilder.php
@@ -2,9 +2,14 @@
 
 namespace telegram_client\HttpClient;
 
+use telegram_client\Dto\ResponseInterface;
 use telegram_client\Service\TelegramServiceInterface;
 
 class ResponseBuilder implements ResponseBuilderInterface
 {
 
+    public function build(): ResponseInterface
+    {
+        // TODO: Implement build() method.
+    }
 }
--- src/HttpClient/ResponseBuilderInterface.php
+++ src/HttpClient/ResponseBuilderInterface.php
@@ -2,9 +2,10 @@
 
 namespace telegram_client\HttpClient;
 
+use telegram_client\Dto\ResponseInterface;
 use telegram_client\Service\TelegramServiceInterface;
 
 interface ResponseBuilderInterface
 {
-
+    public function build(): ResponseInterface;
 }
